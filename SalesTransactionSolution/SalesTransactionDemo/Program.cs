77bc7598f3639f9597343b34f9d5f4f272715272
TLewis <110624677+TerelLewis@users.noreply.github.com>
2023-06-16 12:31:51 -0500

    Add files via upload

---
+++ SalesTransactionSolution/SalesTransactionDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿// Name: Terel Lewis
+// Date: 4/16/2023
+// Description: Application Demo of Sales Transaction.
+
+using System;
+using static System.Console;
+using SalesTransactionLibrary;
+
+
+namespace SalesTransactionDemo
+{
+    class SalesTransactionDemo
+    {
+        static void Main()
+        {
+            SalesTransaction sales1 = new SalesTransaction("Abbott", 2000, 0.2m);
+            SalesTransaction sales2 = new SalesTransaction("Bender", 4000);
+            SalesTransaction sales3 = new SalesTransaction("Carter");
+
+            WriteLine("{0} had sales totaling {1}. Commission rate is {2}; commission value is {3}.", sales1.SalespersonName, sales1.SalesAmount.ToString("F0"), sales1.CommissionRate.ToString("F1"), sales1.Commission.ToString("F0"));
+            WriteLine("{0} had sales totaling {1}. Commission rate is {2}; commission value is {3}.", sales2.SalespersonName, sales2.SalesAmount.ToString("F0"), sales2.CommissionRate.ToString("F1"), sales2.Commission.ToString("F0"));
+            WriteLine("{0} had sales totaling {1}. Commission rate is {2}; commission value is {3}.", sales3.SalespersonName, sales3.SalesAmount.ToString("F0"), sales3.CommissionRate.ToString("F1"), sales3.Commission.ToString("F0"));
+
+
+            decimal totalSalesAmount = sales1.SalesAmount + sales2.SalesAmount + sales3.SalesAmount;
+            decimal totalCommission = sales1.Commission + sales2.Commission + sales3.Commission;
+
+            WriteLine("\nTotal Sales Amount: {0}", totalSalesAmount.ToString("C0"));
+            
+        }
+    }
+
+}
---
+++ SalesTransactionSolution/SalesTransactionLibrary/SalesTransaction.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTransactionLibrary
+{
+    public class SalesTransaction
+    {
+        // Fields
+        private string salespersonName;
+        private decimal salesAmount;
+        private decimal commissionRate;
+
+        //properties
+        public string SalespersonName
+        {
+            get { return salespersonName; }
+            set { salespersonName = value; }
+        }
+
+        public decimal SalesAmount
+        {
+            get { return salesAmount; }
+            set { salesAmount = value; }
+        }
+
+        public decimal Commission
+        {
+            get { return SalesAmount * commissionRate; }
+        }
+
+        public decimal CommissionRate
+        {
+            get { return commissionRate; }
+        }
+
+        // constructors
+        public SalesTransaction(string name, decimal amount = 0, decimal rate = 0)
+        {
+            SalespersonName = name;
+            SalesAmount = amount;
+            commissionRate = rate;
+        }
+
+        public SalesTransaction(string name, decimal amount)
+        {
+            SalespersonName = name;
+            SalesAmount = amount;
+            commissionRate = 0;
+        }
+    }
+}
